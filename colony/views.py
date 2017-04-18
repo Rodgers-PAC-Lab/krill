4b447a4186d5b8dc7b4f116546052116fbef0fe3
Chris Rodgers <xrodgers@gmail.com>
2017-04-18 14:30:25 -0400

    only include cages in 1710 in the current cage counts

--- colony/views.py
+++ colony/views.py
@@ -447,9 +447,11 @@ def summary(request):
 
     # Contains information about only non-defunct cages
     # Exclude empty cages
+    # Include only cages in 1710
     current_table_data = [{ 
         'name': person.name, 
-        'cages': cages.filter(proprietor=person, defunct=False).exclude( 
+        'cages': cages.filter(
+            proprietor=person, defunct=False, location=0).exclude( 
             mouse__isnull=True).count(),
         'mice': mice.filter(cage__proprietor=person, 
             cage__defunct=False).count(),
