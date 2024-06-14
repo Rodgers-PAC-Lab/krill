fdc517b426ebf157e26aef23f0703029e52ad0c0
PAC Lab <rodgers.paclab@gmail.com>
2024-06-14 12:23:27 -0400

    Playing with filtering by proprietor and genotype

--- Census.py
+++ Census.py
@@ -8,3 +8,10 @@ cage_data = []
 for cage in allcages:
     cage_data.append([cage.proprietor,cage.name,cage.sticker,cage.n_mice(),cage.dar_id])
 census_df = pandas.DataFrame(cage_data, columns=('Proprietor','Name','sticker','Number_mice','DAR_ID'))
+Cedric_cages = allcages.filter(proprietor='5')
+# Fetch mice in the cage
+qs = Cedric_cages.prefetch_related('mouse_set').\
+    prefetch_related('mouse_set__mousegene_set')
+for cage in allcages:
+    cage_data.append([cage.proprietor,cage.name,cage.sticker,cage.n_mice(),cage.dar_id])
+census_df = pandas.DataFrame(cage_data, columns=('Proprietor','Name','sticker','Number_mice','DAR_ID'))
--- MitoPark_planning.py
+++ MitoPark_planning.py
@@ -1,12 +1,23 @@
 import colony.models
 import pandas
 
+# Get people to be able to code in the proprietor
+people = colony.models.Person.objects.values_list()
+for person in people:
+    print(person[0:2])
+
+# Cedric = 5, Eliana = 6, Lucas = 8
 Lucas_cages = colony.models.Cage.objects.filter(proprietor_id=8)
 M_1123=Lucas_cages[20]
 M1123_mice=colony.models.Mouse.objects.filter(cage=M_1123)
 
+
 #Cre_Tflox_cages = colony.models.Cage.objects.filter(relevant_genesets=[('DAT-Ires-Cre', 'Tfam-flox')])
-Living_mice = colony.models.Mouse.objects.filter(sacked=False)
+qs = colony.models.Cage.objects.filter(defunct=False)
+attributes = qs.__dict__
+Living_mice = colony.models.Mouse.objects.filter(sack_date__isnull=True)
+Living_mice = Living_mice.prefetch_related('mousegene_set').\
+    prefetch_related('litter')
 
 qset_w_genes = mouse_1101.mousegene_set.all()
 
@@ -18,9 +29,11 @@ all_mousegenes[1].gene_name
 
 all_genes = colony.models.Gene.objects.filter()
 Cre_gene = all_genes.filter(name="DAT-Ires-Cre").get()
+FAD_gene = all_genes.filter(name="5xFAD").get()
+
+mouse1.mousegene_set.filter(gene_name_id=1).get().zygosity
 
 all_litters = colony.models.Litter.objects.filter()
-
 
 # Pseudocode to look at mouse sex in the db for bias over time...
 # Want to separate by breeding cross and/or by breeding pair
