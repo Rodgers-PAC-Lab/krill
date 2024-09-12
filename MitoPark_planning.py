5f7400f139328cda7fa1acbe32a3c1ae10aad536
PAC Lab <rodgers.paclab@gmail.com>
2024-09-12 18:52:23 -0400

    Got some genotyping queries working! Makes a df and outputs a csv with mouse name, sex, age, cage, cre_zygosity, and tflox_zygosity which will make it easier to find mice with the right genotype.

--- MitoPark_planning.py
+++ MitoPark_planning.py
@@ -1,25 +1,73 @@
 import colony.models
 import pandas
+import numpy as np
 
 # Get people to be able to code in the proprietor
-people = colony.models.Person.objects.values_list()
-for person in people:
-    print(person[0:2])
+# people = colony.models.Person.objects.values_list()
+# for person in people:
+#     print(person[0:2])
 
 # Cedric = 5, Eliana = 6, Lucas = 8
 Lucas_cages = colony.models.Cage.objects.filter(proprietor_id=8)
-M_1123=Lucas_cages[20]
-M1123_mice=colony.models.Mouse.objects.filter(cage=M_1123)
+# M_1123=Lucas_cages[20]
+# M1123_mice=colony.models.Mouse.objects.filter(cage=M_1123)
 
 
-#Cre_Tflox_cages = colony.models.Cage.objects.filter(relevant_genesets=[('DAT-Ires-Cre', 'Tfam-flox')])
-qs = colony.models.Cage.objects.filter(defunct=False)
-attributes = qs.__dict__
-Living_mice = colony.models.Mouse.objects.filter(sack_date__isnull=True)
-Living_mice = Living_mice.prefetch_related('mousegene_set').\
-    prefetch_related('litter')
 
-qset_w_genes = mouse_1101.mousegene_set.all()
+Cre_Tflox_cages = colony.models.Cage.objects.filter(relevant_genesets=[('DAT-Ires-Cre', 'Tfam-flox')])
+# qs = colony.models.Cage.objects.filter(defunct=False)
+# attributes = qs.__dict__
+
+Living_mice = colony.models.Mouse.objects.filter(sack_date__isnull=True, cage__isnull=False)
+
+#Living_mice = Living_mice.prefetch_related('mousegene_set').prefetch_related('litter')
+# Cre_mice_l = []
+# for mouse in Living_mice:
+#     if mouse.mousegene_set.exists():
+#         for mg in mouse.mousegene_set.all():
+#             if mg.gene_name.name == "DAT-Ires-Cre":
+#                 gene = mg.gene_name.name
+#                 zygosity = mg.zygosity
+#                 age = mouse.age()
+#                 cage = mouse.cage
+#                 Cre_mice_l.append([mouse,age,cage,gene,zygosity])
+# Cre_mice_df = pandas.DataFrame(Cre_mice_l,columns = ['Mouse','age','cage','gene','zygosity'])
+
+all_MPgene_mice = []
+for mouse in Living_mice:
+    if mouse.mousegene_set.exists():
+        mouses_geneset = []
+        for mg in mouse.mousegene_set.all():
+            mouses_geneset.append(mg.gene_name.name)
+
+        if "DAT-Ires-Cre" in mouses_geneset or 'Tfam-flox' in mouses_geneset:
+            all_MPgene_mice.append([mouse,mouse.genotype])
+all_MPgene_mice = pandas.DataFrame(all_MPgene_mice,columns=['Mouse','genotype'])
+MPgene_mice_l = []
+for mouse in all_MPgene_mice['Mouse']:
+    Cre_zygosity = 'NA'
+    TFlox_zygosity = 'NA'
+    for mg in mouse.mousegene_set.all():
+        if mg.gene_name.name =="DAT-Ires-Cre":
+            Cre_zygosity = mg.zygosity
+        elif mg.gene_name.name == 'Tfam-flox':
+            TFlox_zygosity = mg.zygosity
+        else: print("EXTRA UNKNOWN GENE, HALT!!")
+    MPgene_mice_l.append([mouse,mouse.age(),mouse.sex, mouse.cage,Cre_zygosity,TFlox_zygosity])
+
+MPgene_mice_df = pandas.DataFrame(MPgene_mice_l,columns = ['Mouse','age', 'sex','cage','Cre_zygosity','TFlox_zygosity'])
+
+# MPgene_mice_df['breeding_notes']=''
+# for i in np.arange(0,len(MPgene_mice_df)):
+#     #Look for potential Mitopark dads
+#     mouse = MPgene_mice_df.loc[i]
+#     if mouse['sex'] == 0 and mouse['Cre_zygosity'] == '+/-' and mouse['TFlox_zygosity'] == '+/-':
+#         #This below doesn't actually work for some reason
+#         mouse['breeding_notes'] = 'Potential mitopark dad'
+
+MPdads = MPgene_mice_df.query('sex==0 and Cre_zygosity=="+/-" and TFlox_zygosity=="+/-"')
+MPgene_mice_df.to_csv('MPgene_mice.csv')
+
 
 all_mousegenes=colony.models.MouseGene.objects.filter()
 all_mousegenes[1]
@@ -31,19 +79,14 @@ all_genes = colony.models.Gene.objects.filter()
 Cre_gene = all_genes.filter(name="DAT-Ires-Cre").get()
 FAD_gene = all_genes.filter(name="5xFAD").get()
 
-mouse1.mousegene_set.filter(gene_name_id=1).get().zygosity
+#mouse1.mousegene_set.filter(gene_name_id=1).get().zygosity
 
+parents_l = []
 all_litters = colony.models.Litter.objects.filter()
-
-# Pseudocode to look at mouse sex in the db for bias over time...
-# Want to separate by breeding cross and/or by breeding pair
-# Colony_pups = mice born from our pairs, not ordered from outside
-# Optional date critera and/or particular breeding pair criteria
-# Pup_genesets = unique genesets in colony_pups
-# For each geneset in pup_genesets:
-#   M = count mouses in colony_pups where mouse_geneset=geneset and sex=M
-#   F = Same but for F
-# M_percentage = M/colony_pups * 100 (repeat for F)
-
-# An array of allll the breeding pairs we've had, as mom, dad, and list of litter IDs if I feel ambitious
-
+for litter in all_litters:
+    mom = litter.mother
+    dad = litter.father
+    if [dad, mom] in parents_l:
+        continue
+    else:
+        parents_l.append([dad, mom])
